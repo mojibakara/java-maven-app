6b3f0c564746a0ea37463a77f05f6ae54f5ce820
mojibakara <a33306429@gmail.com>
2022-09-17 23:03:12 -0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 192.168.15.134:8083/java-maven-app:1.2 .'
+        sh 'docker build -t 192.168.15.134:8083/java-maven-app:1.5 .'
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.15.134:8083"
-        sh 'docker push 192.168.15.134:8083/java-maven-app:1.2'
+        sh 'docker push 192.168.15.134:8083/java-maven-app:1.5'
     }
 } 
 
